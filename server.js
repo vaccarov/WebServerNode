cba0654dcbfb7d5e7aed26e57988ea69457bb98b
Victor Vaccaro <victor.vaccaro@ibm.com>
2017-05-17 16:25:47 +0200

    setup heroku port and start script

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -28,6 +28,6 @@ app.get('/about',(req,res) => {
   });
 });
 
-app.listen(3000,() => {
-  console.log('SERVER IS ON');
+app.listen(port,() => {
+  console.log(`SERVER IS ON ${port}`);
 });
